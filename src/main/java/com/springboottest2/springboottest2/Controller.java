793dd4affd747f7c754338a7777bf809fae01fae
Visvajit <visvajitm@gmail.com>
2024-06-04 11:36:47 -0400

    first commit

---
+++ src/main/java/com/springboottest2/springboottest2/Controller.java
@@ -0,0 +1,17 @@
+package com.springboottest2.springboottest2;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api/string")
+    public String getString() {
+        return "Hello World!";
+    }
+    
+    
+}
