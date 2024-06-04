59273a01c16b5016b3c427de2205b8f9742068a7
Visvajit <visvajitm@gmail.com>
2024-06-04 13:50:29 -0400

    Moved the necessary files to the correct locations

--- src/main/java/com/springboottest2/springboottest2/CustomErrorController.java
+++ src/main/java/com/springboottest2/springboottest2/CustomErrorController.java
@@ -4,8 +4,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
